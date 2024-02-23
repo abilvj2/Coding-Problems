8b3bb6c8ae0d3a77cddfd5dbf92159bf316ebcbe
abilvj2 <37629877+abilvj2@users.noreply.github.com>
2024-02-23 13:19:48 -0600

    Update Array-Duplicates.py

--- Array-Duplicates.py
+++ Array-Duplicates.py
@@ -1,3 +1,14 @@
+'''''Leetcode: 217. Contains Duplicate
+Solved
+Easy
+Topics
+Companies
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.''''
+
+
+
+
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hashset = set()
